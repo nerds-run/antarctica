1c555f8a81580079d7d1b2f1c1f26dd219678927
Tulili <tulilirockz@outlook.com>
2024-04-09 00:13:53 -0300

    feat: fix agent-docker should exist

---
+++ nixos/modules/woodpecker/agent-docker.nix
@@ -0,0 +1,45 @@
+{ config, lib, ... }:
+let
+  cfg = config.antarctica.services.woodpecker;
+
+  hasRunner = (name: builtins.elem name cfg.runners);
+in
+{
+  config = lib.mkIf (cfg.enable && hasRunner "docker") {
+    services.woodpecker-agents = {
+      agents.docker = {
+        enable = true;
+
+        environment = {
+          WOODPECKER_SERVER = "localhost:${toString cfg.rpcPort}";
+          WOODPECKER_MAX_WORKFLOWS = "10";
+          WOODPECKER_BACKEND = "docker";
+          WOODPECKER_FILTER_LABELS = "type=docker";
+          WOODPECKER_HEALTHCHECK = "false";
+        };
+
+        environmentFile = [ cfg.sharedSecretFile ];
+
+        extraGroups = [ "docker" ];
+      };
+    };
+
+    # Make sure it is activated in that case
+    my.system.docker.enable = true;
+
+    # FIXME: figure out the issue
+    services.unbound.resolveLocalQueries = false;
+
+    # Adjust runner service for nix usage
+    systemd.services.woodpecker-agent-docker = {
+      after = [ "docker.socket" ]; # Needs the socket to be available
+      # might break deployment
+      restartIfChanged = false;
+      serviceConfig = {
+        BindPaths = [
+          "/var/run/docker.sock"
+        ];
+      };
+    };
+  };
+}
