c7ab47f88af5f61d4f60b78032d229f37b29c75e
Tulili <tulilirockz@outlook.com>
2024-04-09 00:19:51 -0300

    fix(woodpecker): typo?

--- nixos/modules/woodpecker/server.nix
+++ nixos/modules/woodpecker/server.nix
@@ -43,10 +43,10 @@ in
     services.postgresql = {
       enable = true;
       ensureDatabases = [ "woodpecker" ];
-      ensureUsers."woodpecker" = {
+      ensureUsers = [{
         name = "woodpecker";
         ensureClauses.superuser = true;
-      };
+      }];
     };
   };
 }
